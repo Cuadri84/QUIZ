4e67b7c3740ec3862d2cb5189fcf68b125a34f6a
Cuadri84 <davidcuadrillerosaura@gmail.com>
2020-12-30 04:32:02 +0100

    rehecho y logrado el fetch y local storage

--- javascript/resultado.js
+++ javascript/resultado.js
@@ -1,3 +1,33 @@
-const finalScore = document.getElementById('resultadoFinal');
+ 
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+
 finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+//records
+const MAX_HIGH_SCORES = 5;
+saveHighScore = e =>{
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
+};
+
+
